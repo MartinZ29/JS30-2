7413055d2062d36b1b0c3c2e8854d5c5e1f21e66
martinz <martinzhangyx@gmail.com>
2017-07-11 23:44:09 -0700

    minute hand

--- js30-2.js
+++ js30-2.js
@@ -3,5 +3,10 @@ $(document).ready(function(){
         const now = new Date();
         const seconds = now.getSeconds();
         const secondsDegrees = ((seconds/60)*360);
+
+        const mins = now.getMinutes();
+        const minsDegrees = (mins/60) * 360 +90;
+
+        
     }
 })
