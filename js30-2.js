885732a2b02b6fb3cfcd77cb187a6278cfd00f0b
martinz <martinzhangyx@gmail.com>
2017-07-14 17:00:47 -0700

    sechand degree

--- js30-2.js
+++ js30-2.js
@@ -4,8 +4,10 @@ const hourHand = document.querySelector('.hour');
 
     function setDate(){
         const now = new Date();
+
         const seconds = now.getSeconds();
         const secondsDegrees = ((seconds/60)*360);
+        secondHand.style.transform = 'rotate(${secondsDegrees}deg)';
 
         const mins = now.getMinutes();
         const minsDegrees = (mins/60) * 360 +90;
