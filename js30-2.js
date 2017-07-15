fbb5e3d3304b0ee200b50a520e6dec152d29a1da
martinz <martinzhangyx@gmail.com>
2017-07-14 17:04:46 -0700

    transfrom deg

--- js30-2.js
+++ js30-2.js
@@ -7,7 +7,7 @@ const hourHand = document.querySelector('.hour');
 
         const seconds = now.getSeconds();
         const secondsDegrees = ((seconds/60)*360);
-        secondHand.style.transform = 'rotate(${secondsDegrees}deg)';
+        secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
         const mins = now.getMinutes();
         const minsDegrees = (mins/60) * 360 +90;
